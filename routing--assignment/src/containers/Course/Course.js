ca0a9cccff872a6aac07209d0ec128d104d187db
Marta Niemiec <niemiec.marta@hotmail.com>
2019-05-27 23:41:05 +0200

    done assigment

--- routing--assignment/src/containers/Course/Course.js
+++ routing--assignment/src/containers/Course/Course.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 
 class Course extends Component {
     render () {
+        console.log(this.props);
         return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+            <div style={{margin: '50px 100px', backgroundColor: '#fef', padding: '20px'}}>
+                <h2 style={{color: '#444'}}>{this.props.match.params.title}</h2>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
     }
