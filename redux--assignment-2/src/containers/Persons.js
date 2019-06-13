22a99522257ae19e8f458880387794000b8020da
Marta Niemiec <niemiec.marta@hotmail.com>
2019-06-14 00:09:50 +0200

    added local UI state to the AddPerson.js

--- redux--assignment-2/src/containers/Persons.js
+++ redux--assignment-2/src/containers/Persons.js
@@ -30,7 +30,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onPersonAdd: () => dispatch({type: actionTypes.ADD_PERSON}),
+        onPersonAdd: (name, age) => dispatch({type: actionTypes.ADD_PERSON, personData: {name: name, age: age}}),
         onPersonDelete: (id) => dispatch({type: actionTypes.DELETE_PERSON, personId: id})
     };
 }
